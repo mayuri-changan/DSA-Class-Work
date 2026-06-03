39060c714effcb5b495a042dece2dbe5c87ce980
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-03 23:21:55 +0530

    Insertion

---
+++ Insertion.java
@@ -0,0 +1,28 @@
+package com.demo.dsa;
+
+public class Insertion {
+
+	public static void main(String[] args) {
+		int arr[]= {6,3,2,9,1};
+		int temp,j;
+		for(int i=0;i<arr.length;i++)
+		{
+			temp=arr[i];
+			j=i;
+			while(j>0 && arr[j-1]>temp)
+			{ arr[j]=arr[j-1];
+				j=j-1;
+			}
+			arr[j]=temp;
+			
+			
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+
+	}
+
+}
